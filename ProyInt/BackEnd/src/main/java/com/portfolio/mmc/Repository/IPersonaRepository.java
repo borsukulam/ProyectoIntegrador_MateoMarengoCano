0d2d241db5585799b2e248f0fb4ff007a2d25a25
Mateo Marengo Cano <mateomc10@gmail.com>
2022-08-21 09:33:55 -0300

    Final vid 12

---
+++ ProyInt/BackEnd/src/main/java/com/portfolio/mmc/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.mmc.Repository;
+
+import com.portfolio.mmc.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
---
+++ ProyInt/BackEnd/src/main/java/com/portfolio/mmc/Service/ImpPersonaService.java
@@ -0,0 +1,36 @@
+package com.portfolio.mmc.Service;
+
+import com.portfolio.mmc.Entity.Persona;
+import com.portfolio.mmc.Interface.IPersonaService;
+import com.portfolio.mmc.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
