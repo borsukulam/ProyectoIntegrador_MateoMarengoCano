b7703cf415e29e4c3b3f0f45f21913fdbdb2c5f9
Mateo Marengo Cano <mateomc10@gmail.com>
2022-08-22 12:07:48 -0300

    1:23 vid 15

---
+++ ProyInt/BackEnd/src/main/java/com/portfolio/mmc/Security/Controller/AuthController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.mmc.Security.Controller;
+
+import com.portfolio.mmc.Security.Service.RolService;
+import com.portfolio.mmc.Security.Service.UsuarioService;
+import com.portfolio.mmc.Security.jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+}
---
+++ ProyInt/BackEnd/src/main/java/com/portfolio/mmc/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.mmc.Security.Repository;
+
+import com.portfolio.mmc.Security.Entity.Rol;
+import com.portfolio.mmc.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
