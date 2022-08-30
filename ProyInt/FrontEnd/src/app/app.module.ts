6bb18556f04b461d05c75bacdf81d01e14b428f3
Mateo Marengo Cano <mateomc10@gmail.com>
2022-08-30 11:55:40 -0300

    final vid 15

--- ProyInt/FrontEnd/src/app/app.module.ts
+++ ProyInt/FrontEnd/src/app/app.module.ts
@@ -16,7 +16,9 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component'
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -32,15 +34,18 @@ import { LoginComponent } from './components/login/login.component'
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
