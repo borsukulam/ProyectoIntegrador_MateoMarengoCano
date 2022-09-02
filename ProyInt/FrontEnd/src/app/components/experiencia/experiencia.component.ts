829690d1ad89aec72440fcc269f24d74ed6b9d27
Mateo Marengo Cano <mateomc10@gmail.com>
2022-09-02 12:06:57 -0300

    vid 16
    ahora si anda

--- ProyInt/FrontEnd/src/app/components/experiencia/experiencia.component.ts
+++ ProyInt/FrontEnd/src/app/components/experiencia/experiencia.component.ts
@@ -17,6 +17,49 @@ export class ExperienciaComponent implements OnInit {
 
   ngOnInit(): void {
     this.cargarExperiencia();
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  cargarExperiencia(): void {
+    this.sExperiencia.lista().subscribe(data => { this.expe = data; })
+  }
+
+  delete(id?: number){
+    if(id != undefined){
+      this.sExperiencia.delete(id).subscribe(
+        data => {
+          this.cargarExperiencia();
+        }, err => {
+          alert("No se pudo borrar la experiencia");
+        }
+      )
+    }
+  }
+}
+
+/* import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
+
+@Component({
+  selector: 'app-experiencia',
+  templateUrl: './experiencia.component.html',
+  styleUrls: ['./experiencia.component.css']
+})
+export class ExperienciaComponent implements OnInit {
+  expe: Experiencia[] = [];
+
+  constructor(private sExperiencia: SExperienciaService, private tokenService: TokenService) { }
+
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.cargarExperiencia();
     if(this.tokenService.getToken()){
       this.isLogged = true;
     } else {
@@ -40,4 +83,4 @@ export class ExperienciaComponent implements OnInit {
     }
   }
 
-}
+} */
